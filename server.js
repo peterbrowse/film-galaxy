ba2af33a397d9d3590d7f661e119225ca87d4ed4
Peter Browse <peter.browse@bmbagency.com>
2014-03-07 16:53:10 +0000

    Changing prototype to work with top 250 and create local database object.

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ var	blue  		= '\033[34m',
 	reset 		= '\033[0m';
 	
 //Static Variables
-var id_index			= 1,
+var id_index			= 165795,
 	id_prefix			= "tt",
 	url_prefix_start	= 'http://www.imdb.com/title/',
 	url_prefix_end		= '/'
 	id_size				= 7,
-	id_limit			= 10000; 
+	id_limit			= 50000,
+	dynamic_limit		= id_index + id_limit; 
+	
+//tt0165795
 
 //Dynamic Variables
-var id_count	= 0;
+var id_count	= 0,
+	top_list_db	= []
+	db_loaded	= false;
 
 //Dependencies
 var express 	= require('express')
@@ -46,86 +51,119 @@ var pool = poolModule.Pool({
     log : false 
 });
 
-if(id_count < id_index) {
-	id_count++;
-}
-
-for (var i=id_count; i<id_limit+1; i++) {
-	var id_num = id_prefix + pad(i, id_size);
-	var item_url = url_prefix_start+id_prefix+pad(i, id_size)+url_prefix_end;
-	
-	request(item_url, function(error, response, body){
-		if(!error && response.statusCode == 200) {
-			$ = cheerio.load(body);
-			var meta = $('meta');
-   			var keys = Object.keys(meta);
-   			var item_id_url;
-    			
-			keys.forEach(function(key){
-			    if (  meta[key].attribs
-			       	&& meta[key].attribs.property
-			       	&& meta[key].attribs.property === 'og:url') {
-			    		item_id_url = meta[key].attribs.content;
-			    }
-			});
+request('http://www.imdb.com/chart/top', function(error, response, body){
+	if(!error && response.statusCode == 200) {
+		$ = cheerio.load(body);
+		var top_list = [];
+		$('.chart tbody tr .titleColumn a').each(function(i, element) {
+			var href = $(this).attr('href').split('/title/');
+			top_list[i] = href[1];
+		});
+		
+		for (var i = 0; i<top_list.length; i++) {
+			var item_url = url_prefix_start+top_list[i];
 			
-			var item_id				= item_id_url.replace(/.*\/([^/]+).*/, "$1");
-			var item_title			= $('.header span.itemprop').text();
-			var item_year 			= $('.header span.nobr a').text();
-			var item_runtime_raw 	= $('time').text();
-			var item_runtime 		= parseInt(item_runtime_raw.slice(0,item_runtime_raw.indexOf(" min")));
-			var item_rating			= $('#overview-top .star-box .titlePageSprite').text();
-			var item_img_path 		= $("#img_primary .image a img").attr('src');
-			var item_cast_list 		= [];
-			$("[itemprop=actor] a span").each(function(j, element) {
-				item_cast_list[j] = $(this).text();
-			});
-			var item_directors		= [];
-			$('[itemprop=director] a').each(function(j, element) {
-				item_directors[j] = $(this).text();
-			});
-			var item_genres			= [];
-			$('.article [itemprop=genre] a').each(function(j, element) {
-				item_genres[j] = $(this).text();
-			});
-			
-			pool.acquire(function(err, db) {
-				if (err) {
-					console.log(red+"error: "+reset+err);
+			request(item_url, function(error, response, body){
+				if(!error && response.statusCode == 200) {
+					$ = cheerio.load(body);
+					var meta = $('meta');
+		   			var keys = Object.keys(meta);
+		   			var item_id_url;
+		    			
+					keys.forEach(function(key){
+					    if (  meta[key].attribs
+					       	&& meta[key].attribs.property
+					       	&& meta[key].attribs.property === 'og:url') {
+					    		item_id_url = meta[key].attribs.content;
+					    }
+					});
+					
+					var item_id				= item_id_url.replace(/.*\/([^/]+).*/, "$1");
+					var item_title			= $('.header span.itemprop').text();
+					var item_year 			= $('.header span.nobr a').text();
+					var item_rank			= response.request.uri.query.split('ref_=chttp_tt_')[1];
+					var item_runtime_raw 	= $('time').text();
+					var item_runtime 		= parseInt(item_runtime_raw.slice(0,item_runtime_raw.indexOf(" min")));
+					var item_rating			= $('#overview-top .star-box .titlePageSprite').text();
+					var item_img_path 		= $("#img_primary .image a img").attr('src');
+					var item_cast_list 		= [];
+					$("[itemprop=actor] a span").each(function(j, element) {
+						item_cast_list[j] = $(this).text();
+					});
+					var item_directors		= [];
+					$('[itemprop=director] a').each(function(j, element) {
+						item_directors[j] = $(this).text();
+					});
+					var item_genres			= [];
+					$('.article [itemprop=genre] a').each(function(j, element) {
+						item_genres[j] = $(this).text();
+					});
+					
+					top_list_db[item_rank-1] = {
+						title: 		item_title,
+						imdburl: 	item_id_url,
+						imd_img: 	item_img_path,
+						cast: 		item_cast_list,
+						director:	item_directors,
+						genres: 	item_genres,
+						votes: 		item_rating,
+						runtime: 	item_runtime,
+						rating: 	item_rating,
+						year: 		item_year,
+						rank:		item_rank
+					};
+					
+					if(item_rank == top_list.length) {
+						db_loaded = true;
+					}
+					
+					/*
+pool.acquire(function(err, db) {
+						if (err) {
+							console.log(red+"error: "+reset+err);
+						}
+						else {
+							var collection = db.collection('imdb_top_250');
+								collection.update(
+									{_id: item_id},
+									{
+										$set: {
+											title: 		item_title,
+											imdburl: 	item_id_url,
+											imd_img: 	item_img_path,
+											cast: 		item_cast_list,
+											director:	item_directors,
+											genres: 	item_genres,
+											votes: 		item_rating,
+											runtime: 	item_runtime,
+											rating: 	item_rating,
+											year: 		item_year,
+											rank:		item_rank	 
+										}
+									},
+										{ upsert: true },
+										function(err, results) {
+											if(err) {
+												console.log(red+'errr: '+reset+'Adding '+item_title+' into db with error: '+err);
+											} else {
+												console.log(blue+'film: '+reset+item_id+" - "+yellow+item_title+reset+' inserted into database');
+											}
+										}
+								);
+						}
+						pool.release(db);
+					});
+*/
 				}
-				else {
-					var collection = db.collection('imdb_data');
-						collection.update(
-							{_id: item_id},
-							{
-								$set: {
-									title: 		item_title,
-									imdburl: 	item_id_url,
-									imd_img: 	item_img_path,
-									cast: 		item_cast_list,
-									director:	item_directors,
-									genres: 	item_genres,
-									votes: 		item_rating,
-									runtime: 	item_runtime,
-									rating: 	item_rating,
-									year: 		item_year	 
-								}
-							},
-								{ upsert: true },
-								function(err, results) {
-									if(err) {
-										console.log(red+'errr: '+reset+'Adding '+item_title+' into db with error: '+err);
-									} else {
-										console.log(blue+'film: '+reset+item_id+" - "+yellow+item_title+reset+' inserted into database');
-									}
-								}
-						);
-				}
-				pool.release(db);
 			});
 		}
-	});
-}
+	}
+});
+
+
+
+
+
 
 //Functions
 
